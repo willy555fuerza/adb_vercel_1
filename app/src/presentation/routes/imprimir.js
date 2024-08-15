c28c7b49fc3d09eb6dccef3508342154a27e6730
willy555fuerza <willangongales@.com>
2024-08-15 12:04:20 -0400

    adb_vercel_8

--- app/src/presentation/routes/imprimir.js
+++ app/src/presentation/routes/imprimir.js
@@ -118,7 +118,7 @@ router.post('/downloadpdff', async (req, res) => {
         .rect(50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0), rowTop, columnWidths[i], rowHeight)
         .fill();
       doc.fillColor('white')
-        .text(header, 50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0) + 5, rowTop + 3, { width: columnWidths[i] - 10, align: 'center' });
+        .text(header, 50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0) + 5, rowTop + 5, { width: columnWidths[i] - 10, align: 'center' });
     });
 
     rowTop += rowHeight;
@@ -202,31 +202,48 @@ router.post('/downloadpdf', async (req, res) => {
    doc.moveDown(2);
    doc.fontSize(12).text('Detalles del Ingreso por usuario:', { align: 'center', underline: true });
    
-   // Añadir una tabla
-   const tableTop = doc.y + 50; // Centrar verticalmente desde la posición actual
-   const item = data;
+   const pageWidth = doc.page.width; // Ancho total de la página
+   const margin = 50; // Margen izquierdo de la tabla
    
    // Dibujar la tabla
    doc.fontSize(10);
-   const rowHeight = 18; // Reducir aún más la altura de las filas
-   let rowTop = tableTop;
+   const rowHeight = 22; // Aumentar la altura de las filas
+   let rowTop = doc.y + 50; // Posición inicial vertical de la tabla
    
    // Ancho de las columnas adaptado
-   const columnWidths = [60, 110, 110, 110, 60, 60]; // Ajustar los anchos de las columnas
+   const columnWidths = [60, 110, 110, 110, 60, 80]; // Ajustar el ancho de la columna "Fecha" a 80
    
-   // Cabecera de la tabla con colores
+   // Calcular el ancho total de la tabla
+   const tableWidth = columnWidths.reduce((a, b) => a + b, 0);
+   
+   // Calcular la posición horizontal para centrar la tabla
+   const tableLeft = (pageWidth - tableWidth - 2 * margin) / 2 + margin;
+   
+   // Cabecera de la tabla con colores y bordes
    const headers = ['ID Ingreso', 'Usuario', 'Tipo de Ingreso', 'Miembro', 'Monto', 'Fecha'];
    headers.forEach((header, i) => {
+     const xPosition = tableLeft + columnWidths.slice(0, i).reduce((a, b) => a + b, 0);
+   
+     // Dibujar fondo para la celda de la cabecera
      doc.fillColor('blue')
-       .rect(50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0), rowTop, columnWidths[i], rowHeight)
+       .rect(xPosition, rowTop, columnWidths[i], rowHeight)
        .fill();
+   
+     // Dibujar borde gris para la celda de la cabecera
+     doc.strokeColor('gray')
+       .lineWidth(1)
+       .rect(xPosition, rowTop, columnWidths[i], rowHeight)
+       .stroke();
+   
+     // Añadir texto a la celda de la cabecera
      doc.fillColor('white')
-       .text(header, 50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0) + 5, rowTop + 3, { width: columnWidths[i] - 10, align: 'center' });
+       .text(header, xPosition + 5, rowTop + 5, { width: columnWidths[i] - 10, align: 'center' });
    });
    
    rowTop += rowHeight;
    
-   // Datos de la tabla con colores alternados
+   // Datos de la tabla con bordes y colores alternados
+   const item = data;
    const values = [
      item.id_ingreso,
      `${item.usuario_nombres || ''} ${item.usuario_apellidos || ''}`,
@@ -237,11 +254,22 @@ router.post('/downloadpdf', async (req, res) => {
    ];
    
    values.forEach((value, i) => {
+     const xPosition = tableLeft + columnWidths.slice(0, i).reduce((a, b) => a + b, 0);
+   
+     // Dibujar fondo para la celda de datos
      doc.fillColor(i % 2 === 0 ? 'lightgray' : 'white')
-       .rect(50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0), rowTop, columnWidths[i], rowHeight)
+       .rect(xPosition, rowTop, columnWidths[i], rowHeight)
        .fill();
-     doc.fillColor('black')
-       .text(value || '', 50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0) + 5, rowTop + 3, { width: columnWidths[i] - 10, align: 'center' });
+   
+     // Dibujar borde gris para la celda de datos
+     doc.strokeColor('gray')
+       .lineWidth(1)
+       .rect(xPosition, rowTop, columnWidths[i], rowHeight)
+       .stroke();
+   
+     // Añadir texto a la celda de datos
+     doc.fillColor('gray')
+       .text(value || '', xPosition + 5, rowTop + (rowHeight / 2), { width: columnWidths[i] - 10, align: 'center', valign: 'center' });
    });
    
    rowTop += rowHeight;
@@ -249,10 +277,9 @@ router.post('/downloadpdf', async (req, res) => {
    // Asegurarse de que total_ingresos esté definido
    const totalIngresos = item.total_ingresos !== undefined && item.total_ingresos !== null ? item.total_ingresos : 'No disponible';
    
+   // Añadir total de ingresos
    doc.fillColor('red')
-     .text(`Total de Ingresos: ${totalIngresos}`, 50, rowTop);
-   
-    
+     .text(`Total de Ingresos: ${totalIngresos}`, tableLeft, rowTop);
     // Finalizar el documento PDF
     doc.end();
   } catch (error) {
@@ -299,46 +326,70 @@ router.post('/imprimirtablas', async (req, res) => {
     // Agregar datos del ingreso al PDF
     doc.moveDown(2);
     doc.fontSize(12).text('Detalles del Ingreso:', { align: 'center', underline: true });
-   
-    // Añadir una tabla
-    const tableTop = doc.y + 20; // Centrar verticalmente desde la posición actual
-   
-    // Dibujar la tabla
-    doc.fontSize(10);
-    const rowHeight = 18; // Reducir aún más la altura de las filas
-    let rowTop = tableTop;
-   
-    // Ancho de las columnas adaptado
-    const columnWidths = [70, 110, 120, 80,110, 70]; // Ajustar los anchos de las columnas
-   
-    // Cabecera de la tabla con colores
-    const headers = ['ID Ingreso', 'Usuario', 'Tipo de Ingreso','Miembro' ,'Monto', 'Fecha'];
-    headers.forEach((header, i) => {
-      doc.fillColor('blue')
-        .rect(50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0), rowTop, columnWidths[i], rowHeight)
-        .fill();
-      doc.fillColor('white')
-        .text(header, 50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0) + 5, rowTop + 3, { width: columnWidths[i] - 10, align: 'center' });
-    });
-   
     
-  
+    const pageWidth = doc.page.width; // Ancho total de la página
+    const margin = 50; // Margen izquierdo de la tabla
+    
+    // Ancho de las columnas ajustado
+    const columnWidths = [50, 80, 150, 100, 80, 100]; // Reducir el ancho de "Monto" y aumentar el ancho de "Miembro"
+    
+    // Calcular el ancho total de la tabla
+    const tableWidth = columnWidths.reduce((a, b) => a + b, 0);
+    
+    // Calcular la posición horizontal para centrar la tabla
+    const tableLeft = (pageWidth - tableWidth - 2 * margin) / 2 + margin;
+    
+    // Ajustar altura de las filas
+    const rowHeight = 40; // Aumentar la altura de las filas para permitir más espacio para el texto
+    let rowTop = doc.y + 20; // Posición inicial vertical de la tabla
+    
+    // Cabecera de la tabla con colores y bordes
+    const headers = ['ID Ingreso', 'Usuario', 'Tipo de Ingreso', 'Miembro', 'Monto', 'Fecha'];
+    headers.forEach((header, i) => {
+      const xPosition = tableLeft + columnWidths.slice(0, i).reduce((a, b) => a + b, 0);
+    
+      // Dibujar fondo para la celda de la cabecera
+      doc.fillColor('blue')
+        .rect(xPosition, rowTop, columnWidths[i], rowHeight)
+        .fill();
+    
+      // Dibujar borde para la celda de la cabecera
+      doc.strokeColor('gray')
+        .lineWidth(1)
+        .rect(xPosition, rowTop, columnWidths[i], rowHeight)
+        .stroke();
+    
+      // Añadir texto a la celda de la cabecera
+      doc.fillColor('white')
+        .text(header, xPosition + 5, rowTop + (rowHeight / 2) - 5, { width: columnWidths[i] - 10, align: 'center' });
+    });
+    
     rowTop += rowHeight;
-
-    // Datos de la tabla con colores alternados
+    
+    // Datos de la tabla con bordes y colores alternados
     datosTabla.forEach((row) => {
       // Reordenar los datos para que el segundo dato (Usuario) se mueva al final
-      const reorderedRow = [row[0], row[2], row[3],row[4], row[5], row[1]];
-      
-      // Dibujar cada celda en la fila
+      const reorderedRow = [row[0], row[2], row[3], row[4], row[5], row[1]];
+    
       reorderedRow.forEach((cell, i) => {
-        doc.fillColor('black')
-          .text(cell, 50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0) + 5, rowTop + 3, { width: columnWidths[i] - 10, align: 'center' });
+        const xPosition = tableLeft + columnWidths.slice(0, i).reduce((a, b) => a + b, 0);
+    
+        // Dibujar borde para la celda de datos
+        doc.strokeColor('gray')
+          .lineWidth(1)
+          .rect(xPosition, rowTop, columnWidths[i], rowHeight)
+          .stroke();
+    
+        // Añadir texto a la celda de datos
+        doc.fillColor('gray')
+          .text(cell, xPosition + 5, rowTop + (rowHeight / 2) - 5, { width: columnWidths[i] - 10, align: 'center' });
       });
-      // Mover la posición y hacia abajo para la siguiente fila
+    
+      // Mover la posición hacia abajo para la siguiente fila
       rowTop += rowHeight;
     });
-
+    
+    // Aumentar la altura adicional si es necesario para el contenido
     rowTop += rowHeight;
     
     // Finalizar el documento PDF
@@ -386,44 +437,67 @@ router.post('/imprimirtabla', async (req, res) => {
     // Agregar datos del ingreso al PDF
     doc.moveDown(2);
     doc.fontSize(12).text('Detalles del Egreso:', { align: 'center', underline: true });
-   
+    
     // Añadir una tabla
     const tableTop = doc.y + 20; // Centrar verticalmente desde la posición actual
-   
+    
     // Dibujar la tabla
     doc.fontSize(10);
-    const rowHeight = 18; // Reducir aún más la altura de las filas
+    const rowHeight = 35; // Aumentar la altura de las filas
     let rowTop = tableTop;
-   
-    // Ancho de las columnas adaptado
-    const columnWidths = [60, 110, 120, 110, 110]; // Ajustar los anchos de las columnas
-   
-    // Cabecera de la tabla con colores
+    
+    // Reducir el ancho de las columnas para que ocupen menos espacio horizontal
+    const columnWidths = [60, 110, 120, 110, 110]; 
+    
+    // Cabecera de la tabla con colores y bordes
     const headers = ['ID Egreso', 'Usuario', 'Tipo de Egreso', 'Monto', 'Fecha'];
     headers.forEach((header, i) => {
-      doc.fillColor('blue')
-        .rect(50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0), rowTop, columnWidths[i], rowHeight)
-        .fill();
-      doc.fillColor('white')
-        .text(header, 50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0) + 5, rowTop + 3, { width: columnWidths[i] - 10, align: 'center' });
-    });
-   
-    rowTop += rowHeight;
-  
-    datosTabla.forEach((row) => {
-      // Reordenar los datos para que el segundo dato (Usuario) se mueva al final
-      const reorderedRow = [row[0], row[2], row[3],row[4], row[1]];
-      
-      // Dibujar cada celda en la fila
-      reorderedRow.forEach((cell, i) => {
-        doc.fillColor('black')
-          .text(cell, 50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0) + 5, rowTop + 3, { width: columnWidths[i] - 10, align: 'center' });
-      });
-      
-      // Mover la posición y hacia abajo para la siguiente fila
-      rowTop += rowHeight;
+        const xPosition = 50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0);
+        
+        // Dibujar el fondo de la cabecera
+        doc.fillColor('blue')
+            .rect(xPosition, rowTop, columnWidths[i], rowHeight)
+            .fill();
+        
+        // Dibujar el borde alrededor de la celda
+         // Dibujar borde para la celda de datos
+         doc.strokeColor('gray')
+         .lineWidth(1)
+         .rect(xPosition, rowTop, columnWidths[i], rowHeight)
+         .stroke();
+        
+        // Dibujar el texto de la cabecera
+        doc.fillColor('white')
+            .text(header, xPosition + 5, rowTop + 12, { width: columnWidths[i] - 10, align: 'center' });
     });
     
+    rowTop += rowHeight;
+    
+    // Dibujar las filas de datos con bordes
+    datosTabla.forEach((row) => {
+        // Reordenar los datos para que el segundo dato (Usuario) se mueva al final
+        const reorderedRow = [row[0], row[2], row[3], row[4], row[1]];
+        
+        // Dibujar cada celda en la fila
+        reorderedRow.forEach((cell, i) => {
+            const xPosition = 50 + columnWidths.slice(0, i).reduce((a, b) => a + b, 0);
+            
+            // Dibujar el borde alrededor de la celda
+            
+            doc.lineWidth(1) // Ancho del borde
+                .rect(xPosition, rowTop, columnWidths[i], rowHeight)
+                .stroke();
+            
+            // Dibujar el texto de la celda
+            doc.fillColor('gray')
+                .text(cell, xPosition + 5, rowTop + 12, { width: columnWidths[i] - 10, align: 'center' });
+        });
+        
+        // Mover la posición y hacia abajo para la siguiente fila
+        rowTop += rowHeight;
+    });
+    
+      
     // Finalizar el documento PDF
     doc.end();
   } catch (error) {
